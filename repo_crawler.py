d5fe389d6989a09ea3d00da3d3fac5bd85d6383e
avidit <aabishkar@gmail.com>
2017-05-14 22:01:29 -0400

    Add methods to get repo contents

--- repo_crawler.py
+++ repo_crawler.py
@@ -1,3 +1,4 @@
+import base64
 from github import Github
 
 
@@ -6,10 +7,18 @@ class RepoCrawler(object):
     def __init__(self, github_token):
         self.github_client = Github(github_token)
 
+    def get_user(self):
+        """Returns user object"""
+        return self.github_client.get_user()
+
+    def get_repository(self, repository):
+        """Returns repo object of repository"""
+        repos = self.github_client.get_user().get_repos()
+        return next((repo for repo in repos if repo.name == repository), None)
+
     def get_repositories(self, organization):
         """Returns list of repository objects belonging to an organization"""
-        org = self.github_client.get_organization(organization)
-        return [repo for repo in org.get_repos()]
+        return self.github_client.get_organization(organization).get_repos()
 
     def get_branches(self, repository):
         """Returns list of branch objects belonging to a repository"""
@@ -21,3 +30,17 @@ class RepoCrawler(object):
         """Returns all repositories  with their branches in an organization"""
         repos = self.get_repositories(organization)
         return [self.get_branches(repo.full_name) for repo in repos]
+
+    def get_repository_contents(self, repository):
+        """Return array of content objects in a repository"""
+        return self.get_repository(repository).get_contents('.')
+
+    def get_files(self, repository):
+        """Return array of files in a repository root"""
+        return [content for content in self.get_repository_contents(
+            repository) if content.type == 'file']
+
+    def get_file_content(self, repository, file):
+        """Return decoded file content for a file in a repository"""
+        file = self.get_repository(repository).get_file_contents(file)
+        return base64.b64decode(file.content).decode('utf-8')
